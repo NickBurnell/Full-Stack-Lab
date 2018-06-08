614df3020605af109b039965e44136a4e99145e1
Nick Burnell <nicholas.burnell92@gmail.com>
2018-06-08 14:30:07 -0400

    added increment and decrement button for quantity, and finnished styles

--- public/js/components/cart-items.js
+++ public/js/components/cart-items.js
@@ -5,14 +5,20 @@ const cartItems = {
     <h1>Shopify Your Life</h1>
     <section class="main">
         <section ng-repeat="item in $ctrl.cartItems" class="dataDisplay">
-            <h3>{{ item.product }}</h3>
-            <p>Price: {{ item.price }}</p>
-            <p>Quantity: {{ item.quantity }}</p>
-            <a href="" ng-click="$ctrl.deleteItem(item.id);" class="delete">Delete</a>
+            <div class="iconWrapper">
+                <h3>{{ item.product }}</h3>
+                <i ng-click="$ctrl.deleteItem(item.id);" class="material-icons">clear</i>
+            </div>
+            <p>Price: $ {{ item.price }}</p>
+            <div class="quantityUpdate">
+                <button type="button" ng-click="$ctrl.decrement(item)"> - </button>
+                <p>Quantity: {{ item.quantity }}</p>
+                <button type="button" ng-click="$ctrl.increment(item)"> + </button>
+            </div>
         </section>
     </section>
 
-        <form ng-submit="$ctrl.addItem($ctrl.newItem);" class="form">
+        <form ng-submit="$ctrl.addItems($ctrl.newItem);" class="form">
             <h5>Add New Item</h5>
             <input type="text" placeholder="Product Name.." ng-model="$ctrl.newItem.product">
             <input type="number" placeholder="Price.." ng-model="$ctrl.newItem.price">
@@ -27,6 +33,7 @@ const cartItems = {
         });
         vm.addItems = (newItem) => {
             CartService.addItem(newItem).then((response) => {
+                console.log("ive been clicked");
               vm.cartItems = response.data;
             });
             vm.newItem = {};
@@ -40,6 +47,16 @@ const cartItems = {
             CartService.updateItem(item).then((response) => {
               vm.cartItems = response.data;
             });
+        };
+        vm.increment = (item) => {
+            item.quantity++;
+            vm.updateItem(item);
+        };
+        vm.decrement = (item) => {
+            if (item.quantity > 0) {
+                item.quantity--;
+                vm.updateItem(item);
+            }
         };
     }]
 };
